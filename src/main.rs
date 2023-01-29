59fdf756820db3b3520f00623ad623f1576bbc36
MaxOhn <ohn.m@hotmail.de>
2023-01-29 04:28:04 +0100

    added tests

--- src/main.rs
+++ src/main.rs
@@ -176,3 +176,68 @@ impl Buttons {
         (self.0 & Self::K2) > 0
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn compare_ur(map_file: &str, replay_file: &str, expected: f64) {
+        let map_path = format!("./test-data/{map_file}.osu");
+        let replay_path = format!("./test-data/{replay_file}.osr");
+
+        let map = Beatmap::from_path(map_path).expect("failed to parse map");
+        let replay = Replay::from_file(replay_path).expect("failed to parse replay");
+
+        let ur = (100.0 * calculate_ur(&map, &replay)).round() / 100.0;
+
+        assert!(
+            (ur - expected).abs() <= f64::EPSILON,
+            "expected {expected}, got {ur}",
+        );
+    }
+
+    #[test]
+    fn respektive_unforgiving() {
+        compare_ur(
+            "Within Temptation - The Unforgiving (Armin) [Marathon]",
+            "replay-osu_156352_3460700148",
+            130.37,
+        )
+    }
+
+    #[test]
+    fn gn_unforgiving() {
+        compare_ur(
+            "Within Temptation - The Unforgiving (Armin) [Marathon]",
+            "replay-osu_156352_3549163348",
+            135.48,
+        )
+    }
+
+    #[test]
+    fn respektive_haitai() {
+        compare_ur(
+            "Ayase Rie - Yuima-ruWorld TVver. (Fycho) [Extra]",
+            "replay-osu_983680_2294262584",
+            87.15,
+        )
+    }
+
+    #[test]
+    fn respektive_high_powered() {
+        compare_ur(
+            "sphere - HIGH POWERED (TV Size) (Azunyan-) [POWER OVERLOAD EXPERT]",
+            "replay-osu_2779503_3916842208",
+            90.80,
+        )
+    }
+
+    #[test]
+    fn respektive_whos_world() {
+        compare_ur(
+            "Euchaeta - Who's World (P_O) [Who Does This World Belong To]",
+            "replay-osu_3312004_4205640222",
+            96.14,
+        )
+    }
+}
