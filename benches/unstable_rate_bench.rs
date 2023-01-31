47daed84f1c5992c7cafd6a460c5a1952f45d2ce
MaxOhn <ohn.m@hotmail.de>
2023-02-01 00:59:10 +0100

    adjusted benchmark

--- benches/unstable_rate_bench.rs
+++ benches/unstable_rate_bench.rs
@@ -3,7 +3,7 @@ use std::hint::black_box;
 use criterion::{criterion_group, criterion_main, Criterion};
 use osu_db::Replay;
 use rosu_pp::Beatmap;
-use rosu_ur_calc::{calculate_ur, calculate_ur_baseline};
+use rosu_ur_calc::*;
 
 pub fn unstable_rate_bench(c: &mut Criterion) {
     let map_file = "Ayase Rie - Yuima-ruWorld TVver. (Fycho) [Extra]";
@@ -17,18 +17,38 @@ pub fn unstable_rate_bench(c: &mut Criterion) {
     let map_file = "Euchaeta - Who's World (P_O) [Who Does This World Belong To]";
     let replay_file = "respektive_whos_world";
     new_group(c, "respektive whos world", map_file, replay_file);
+
+    let map_file = "Chroma - sink to the deep sea world (None1637) [AR10]";
+    let replay_file = "respektive_sink";
+    new_group(c, "respektive sink", map_file, replay_file);
+
+    let map_file = "Feryquitous - Central Nucleus (Shiirn) [Sanctus Nexum]";
+    let replay_file = "rohulk_sanctus";
+    new_group(c, "rohulk sanctus", map_file, replay_file);
 }
 
 fn new_group(c: &mut Criterion, name: &str, map_file: &str, replay_file: &str) {
     let (map, replay) = parse_map_replay(map_file, replay_file);
     let mut group = c.benchmark_group(name);
 
-    group.bench_with_input("baseline", &(&map, &replay), |b, &(map, replay)| {
-        b.iter(|| calculate_ur_baseline(black_box(map), black_box(replay)))
+    // group.bench_with_input("baseline", &(&map, &replay), |b, &(map, replay)| {
+    //     b.iter(|| calculate_ur_baseline(black_box(map), black_box(replay)))
+    // });
+
+    // group.bench_with_input("iters", &(&map, &replay), |b, (map, replay)| {
+    //     b.iter(|| calculate_ur_iters(black_box(map), black_box(replay)))
+    // });
+
+    group.bench_with_input("stable", &(&map, &replay), |b, (map, replay)| {
+        b.iter(|| calculate_ur_stable(black_box(map), black_box(replay)))
     });
 
-    group.bench_with_input("candidate", &(&map, &replay), |b, (map, replay)| {
-        b.iter(|| calculate_ur(black_box(map), black_box(replay)))
+    group.bench_with_input("custom1", &(&map, &replay), |b, (map, replay)| {
+        b.iter(|| calculate_ur_custom1(black_box(map), black_box(replay)))
+    });
+
+    group.bench_with_input("custom2", &(&map, &replay), |b, (map, replay)| {
+        b.iter(|| calculate_ur_custom2(black_box(map), black_box(replay)))
     });
 
     group.finish();
--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,14 @@ mod models;
 use osu_db::Replay;
 use rosu_pp::Beatmap;
 
-pub use impls::baseline::calculate_ur as calculate_ur_baseline;
+pub use impls::{
+    baseline::calculate_ur as calculate_ur_baseline,
+    circleguard::calculate_ur as calculate_ur_circleguard,
+    custom1::calculate_ur as calculate_ur_custom1, custom2::calculate_ur as calculate_ur_custom2,
+    iters::calculate_ur as calculate_ur_iters, stable::calculate_ur as calculate_ur_stable,
+};
 
-macro_rules! export_fns {
+macro_rules! default_fn {
     ( $( $feature:literal -> $module:ident ,)* ) => {
         $(
             #[cfg(feature = $feature)]
@@ -22,7 +27,7 @@ macro_rules! export_fns {
     }
 }
 
-export_fns! {
+default_fn! {
     "baseline" -> baseline,
     "iters" -> iters,
     "stable" -> stable,
